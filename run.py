b50801f0ececebee1ab1b08c972eca28dc574fd7
Ozymo Alt <nvhai.contact@gmail.com>
2024-07-05 17:34:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     print("Press F2 to exit the program")
     print("Press F3 to pause/continue the program")
     print("Press F4 to reload config")
-    print("For any questions, join my discord: https://discord.gg/C3MY4kuAcD")
+   # print("For any questions, join my discord: https://discord.gg/C3MY4kuAcD")
     print("-" * 50)
 
     bot = TriggerBot()
